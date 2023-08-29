16b4aecd893c032f07a4e91596e5554428a4baf0
AlexandraTrifan <Alexandra.Trifan@analog.com>
2023-08-29 15:54:07 +0300

    CommandQueue: Rework the entire mechanism of the command queue.
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- iioutil/src/commandqueue.cpp
+++ iioutil/src/commandqueue.cpp
@@ -14,9 +14,6 @@ CommandQueue::CommandQueue(int numberOfThreads, QObject *parent)
 	: QObject(parent)
 	, m_running(false)
 	, m_nbThreads(numberOfThreads)
-	, m_async(m_nbThreads > 1)
-	, m_workNewThread(m_nbThreads != 0)
-	, m_currentCommand(nullptr)
 {
 	m_commandExecThreadPool.setMaxThreadCount(std::min(m_nbThreads, QThread::idealThreadCount()));
 }
@@ -24,7 +21,6 @@ CommandQueue::CommandQueue(int numberOfThreads, QObject *parent)
 CommandQueue::~CommandQueue()
 {
 	requestStop();
-	wait();
 
 	for(auto c : m_commandQueue) {
 		delete c;
@@ -34,96 +30,54 @@ CommandQueue::~CommandQueue()
 
 void CommandQueue::enqueue(Command *command)
 {
-	std::lock_guard<std::mutex> lock(m_commandMutex);
-	start();
-	connect(command, &Command::started, this, &CommandQueue::cmdStarted,
-		static_cast<Qt::ConnectionType>(Qt::QueuedConnection | Qt::UniqueConnection));
-	connect(command, &Command::finished, this, &CommandQueue::cmdFinished,
-		static_cast<Qt::ConnectionType>(Qt::QueuedConnection | Qt::UniqueConnection));
 	m_commandQueue.push_back(command);
+	qDebug(CAT_COMMANDQUEUE) << "enqueued " << command;
+
+	if(!m_running) {
+		start();
+	}
 }
 
 void CommandQueue::start()
 {
-	if(!m_running) {
-		qDebug(CAT_COMMANDQUEUE) << "CommandQueue set running to true (start)";
-		m_running = true;
-		if(m_workNewThread) {
-			m_commandExecThreadPool.start(std::bind(&CommandQueue::work, this));
-		} else {
-			// trigger work on Main Thread
-			QMetaObject::invokeMethod(this, "work", Qt::QueuedConnection);
-		}
+	m_running = true;
+	runCmd();
+}
+
+void CommandQueue::resolveNext(scopy::Command *cmd)
+{
+	m_commandQueue.pop_front(); // also delete/disconnect
+	qDebug(CAT_COMMANDQUEUE) << "delete " << cmd;
+	disconnect(cmd, &Command::finished, this, &CommandQueue::resolveNext);
+	cmd->deleteLater();
+
+	if(m_commandQueue.size() == 0) {
+		m_running = false;
+
+	} else {
+		runCmd();
+	}
+}
+
+void CommandQueue::runCmd()
+{
+	qDebug(CAT_COMMANDQUEUE) << "run cmd " << m_commandQueue.size();
+	if(m_running) {
+		connect(m_commandQueue.at(0), &Command::finished, this, &CommandQueue::resolveNext);
+		QtConcurrent::run(&m_commandExecThreadPool, std::bind([=]() {
+			qDebug(CAT_COMMANDQUEUE) << "execute start " << m_commandQueue.at(0);
+			m_commandQueue.at(0)->execute();
+			qDebug(CAT_COMMANDQUEUE) << "execute stop " << m_commandQueue.at(0);
+		}));
 	}
 }
 
 void CommandQueue::requestStop()
 {
-	if(m_running) {
-		qDebug(CAT_COMMANDQUEUE) << "CommandQueue set running to false (stop)";
-		m_running = false;
-		if(m_workNewThread) {
-			std::unique_lock<std::mutex> lock(m_commandMutex);
-			m_commandQueue.clear();
-		}
-	}
+	qDebug(CAT_COMMANDQUEUE) << "request stop " << m_commandQueue.size();
+	m_running = false;
 }
 
-void CommandQueue::wait()
-{
-	if(m_running) {
-		qDebug(CAT_COMMANDQUEUE) << "CommandQueue set running to true (wait)";
-		m_running = false;
-	}
-	if(m_workNewThread) {
-		m_commandExecThreadPool.waitForDone();
-	}
-}
-
-void CommandQueue::work()
-{
-	while(m_running) {
-		std::unique_lock<std::mutex> lock(m_commandMutex);
-		if(m_commandQueue.empty()) {
-			m_running = false;
-			break;
-		}
-
-		m_currentCommand = m_commandQueue.front();
-		if(m_async) {
-			QtConcurrent::run(&m_commandExecThreadPool, std::bind([=]() { m_currentCommand->execute(); }));
-		} else {
-			int size = m_commandQueue.size();
-			lock.unlock();
-			qDebug(CAT_COMMANDQUEUE)
-				<< "CommandQueue executing " << m_currentCommand << " cmdq size: " << size;
-			m_currentCommand->execute();
-			m_currentCommand->deleteLater();
-			m_currentCommand = nullptr;
-			lock.lock();
-		}
-		m_commandQueue.pop_front();
-	}
-}
-
-void CommandQueue::cmdFinished(scopy::Command *cmd)
-{
-	if(!cmd) {
-		cmd = dynamic_cast<Command *>(QObject::sender());
-	}
-	if(cmd) {
-		Q_EMIT finished(cmd);
-	}
-}
-
-void CommandQueue::cmdStarted(scopy::Command *cmd)
-{
-	if(!cmd) {
-		cmd = dynamic_cast<Command *>(QObject::sender());
-	}
-	if(cmd) {
-		Q_EMIT started(cmd);
-	}
-}
+void CommandQueue::wait() {}
 
 #include "moc_commandqueue.cpp"
